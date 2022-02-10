b81a8f5dfa88571331f745ad26ee8efe8fa102e9
Mathieu Dielna <mathieudielna.pro@gmail.com>
2022-02-10 01:38:56 +0100

    retrait comment et code inutiles

--- src/main/java/com/alumni/spring/controllers/InscriptionController.java
+++ src/main/java/com/alumni/spring/controllers/InscriptionController.java
@@ -2,21 +2,17 @@ package com.alumni.spring.controllers;
 
 import com.alumni.spring.models.Utilisateur;
 import com.alumni.spring.service.UtilisateurService;
-import org.springframework.beans.factory.annotation.Autowired;
+
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.*;
 import javax.validation.Valid;
-import java.util.Date;
+
 
 @Controller
 public class InscriptionController {
 
-    /*
-    * https://www-baeldung-com.translate.goog/spring-mvc-form-tutorial?_x_tr_sl=auto&_x_tr_tl=fr&_x_tr_hl=fr
-    * @Autowired
-    */
     UtilisateurService utilisateurService;
 
     @GetMapping("/inscription")
@@ -53,23 +49,8 @@ public class InscriptionController {
         return "info";
         */
    }
-    }
-/*    @RequestMapping(value ="/ajouterUtilisateur", method = RequestMethod.POST)
-    public String inscriptionUtilisateur(final @Valid @ModelAttribute("utilisateur") Utilisateur infoUser, final BindingResult bindingResult, final Model model)
-    {
-        if(bindingResult.hasErrors()){
-            model.addAttribute("registrationForm", infoUser);
-            return "inscription";
-        }
-        try {
-            utilisateurService.inscription(infoUser);
-        }catch (Exception e){
-            bindingResult.rejectValue("email", "userData.email","An account already exists for this email.");
-            model.addAttribute("registrationForm", infoUser);
-            return "inscription";
-        }
-        return "validInscription";
-    }*/
+}
+
 
 
 
